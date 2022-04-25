ff96564cf003b22ec9bc7e2bdfedfa6b62cf3a70
Ivan Stukalin <ivanstukalin@gmail.com>
2022-04-25 07:05:50 +0300

    Добавил новые функции в ContractOrganizer

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,34 +2,151 @@
 
 pragma solidity ^0.8.4;
 
-contract Voting {
+contract VotingOrganizer {
 
-    struct Vote {
+    struct Voting {
         uint256   id;
         uint      creationDate;
         address[] candidateList;
+        uint      moneyReceived;
         bool      isFinished;
     }
 
-    address private owner;
-    Vote[] private votes;
+    uint     public votingPrice;
+    uint     public commission;
+
+    address  private _owner;
+    Voting[] private _votings;
+
+    mapping(uint256 => mapping (address => uint256)) _results;
+    mapping(uint256 => address[]) _votedVoters;
 
     constructor() {
-        owner = msg.sender;
+        _owner      = msg.sender;
+        votingPrice = 0.01 ether;
+        commission  = 10;
     }
 
-    function addVoting(address[] memory addressesOfCandidates) ownerAccess public payable {
-        uint256 voteId = votes.length + 1;
-        Vote memory vote = Vote(voteId, block.timestamp, addressesOfCandidates, false);
-        votes.push(vote);
+    function addVoting(address[] memory addressesOfCandidates) ownerAccess public {
+        uint256 votingId = _votings.length;
+        
+        Voting memory newVoting = Voting(votingId, block.timestamp, addressesOfCandidates, 0, false);
+        _votings.push(newVoting);
+        
+        for (uint256 i = 0; i < addressesOfCandidates.length; i++) {
+            _results[votingId][addressesOfCandidates[i]] = 0;
+        }
     }
 
-    function getVoting() public view returns (Vote[] memory) {
-       return votes;
+    function vote(uint256 votingId, address candidate) votingActvie(votingId) public payable {
+        require(msg.value >= votingPrice, "Sum must be greater than 0,01 ETH"); 
+        Voting memory voting = _getVoting(votingId); 
+        _isCandidateInVoting(voting, candidate);
+        require(!_hasVoterAlreadyVote(voting.id, msg.sender), "You have already voted!");
+        
+        _votings[voting.id].moneyReceived = _votings[voting.id].moneyReceived + msg.value;
+        ++_results[voting.id][candidate];
+        _votedVoters[voting.id].push(msg.sender);
+    }
+
+    function getVotings() public view returns (Voting[] memory) {
+       return _votings;
+    }
+
+    function getVotingResults(uint256 votingId, address candidate) public view returns (uint256){
+        Voting memory voting = _getVoting(votingId); 
+        _isCandidateInVoting(voting, candidate);
+        return _results[voting.id][candidate];
+    }
+
+    function getVotingCandidateList(uint256 votingId) public view returns (address[] memory) {
+        Voting memory voting = _getVoting(votingId);
+        return voting.candidateList;
+    }
+
+    function finishVoting(uint votingId) votingActvie(votingId) public returns (address) {
+        Voting memory voting = _getVoting(votingId);
+        require(voting.creationDate < block.timestamp + 3 days, "The voting has not been finished yet.");
+        
+        address winnerAddress = getWinner(voting);
+        address payable _to   = payable(winnerAddress);
+        uint sum              = voting.moneyReceived/(commission/100);
+        
+        _to.transfer(sum);
+
+        voting.isFinished = true;
+        return winnerAddress;
+    }
+
+    function withdrawCommision() ownerAccess public {
+        address payable _to   = payable(_owner);
+        address _thisContract = address(this);  
+
+        _to.transfer(_thisContract.balance);
+    }
+
+    function getWinner(Voting memory voting) private view returns (address) {
+        uint256 winningVoteCount = 0;
+        uint256 voteCount        = 0;
+        uint256 winnerId         = 0;
+        bool isSingleWinner      = true;
+        for (uint256 p = 0; p < voting.candidateList.length; p++) {
+            voteCount = _results[voting.id][voting.candidateList[p]];
+            if (voteCount == winningVoteCount) {
+                isSingleWinner = false;
+            }
+            if (voteCount > winningVoteCount) {
+                winningVoteCount = voteCount;
+                winnerId         = voting.id;
+                isSingleWinner   = true;
+            }
+        }
+        require(winningVoteCount != 0, "There are no one voted");
+        require(isSingleWinner, "There must be one winner in the voting.");
+        return voting.candidateList[winnerId];
+    }
+
+    function _hasVoterAlreadyVote(uint256 votingId, address voter) private view returns (bool) {
+        for (uint256 i = 0; i < _votedVoters[votingId].length; i++) {
+            if (_votedVoters[votingId][i] == voter) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function _getVoting(uint256 votingId) private view returns (Voting memory) {
+        if (votingId == 0) {
+            return _votings[0];
+        }
+        
+        uint256 id = 0;
+        for (uint256 i; i < _votings.length; i++) {
+            if (_votings[i].id == votingId) {
+                id = votingId;
+            }
+        } 
+        require(id > 0, "Unexpected voting id");
+        return _votings[id];
+    }
+
+    function _isCandidateInVoting(Voting memory voting, address candidate) private pure {
+        bool isCandidateInVoting = true;
+        for (uint256 i; i < voting.candidateList.length; i++) {
+            if (voting.candidateList[i] == candidate) {
+                return;
+            }
+        }
+        require(isCandidateInVoting, "Candidate does not exist");
     }
 
     modifier ownerAccess() {
-        require(msg.sender == owner);
+        require(msg.sender == _owner, "Has no right to add voting");
+        _;
+    }
+
+    modifier votingActvie(uint256 votingId) {
+        require(_votings[votingId].isFinished == false, "Voting has been finished!");
         _;
     }
 }
